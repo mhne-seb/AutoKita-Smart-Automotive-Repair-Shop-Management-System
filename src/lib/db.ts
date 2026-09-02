7e46b79b10ae5fa35a3a299508ae21659eeff4ba
lm-ace <lmiguelaceitunas@gmail.com>
2026-09-02 13:22:23 +0800

    connect with supa

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,6 @@
 // src/lib/db.ts
 import { Pool } from 'pg';
+import fs from 'fs';
 
 export const db = new Pool({
   host: process.env.DB_HOST,
@@ -7,5 +8,9 @@ export const db = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: parseInt(process.env.DB_PORT || '5432', 10),
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('certs/prod-ca-2021.crt').toString(),
+  }
 });
 
